b293ae07d78b13f7cdf3cd533dc570ad29d7904e
Scott <scottalankatz@gmail.com>
2014-10-22 21:24:02 -0400

    Initial commit. Whoa

---
+++ spec/features/helper.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+require 'capybara/rails'
+
+RSpec.configure do |config|
+  config.include Rails.application.routes.url_helpers
+end
+
+
---
+++ spec/features/user_account_spec.rb
@@ -0,0 +1,22 @@
+require 'features/helper'
+
+describe "User Account Spec" do
+   describe "Creating an Account" do
+    it "Persists the user in the database" do
+      visit root_path
+      click_link_or_button "#create_account"
+      within "#user_form" do
+        fill_in ".user_name", with: "scott"
+        fill_in ".email_address", with: "scott@example.com"
+        fill_in ".password", with: "password"
+        fill_in ".password_confirm", with: "password"
+
+        click_link_or_button ".create_user"
+      end
+
+      expect(page).to have_content("Account created for scott@example")
+      expect(User.exists?(email_address: "scott@example.com")).to be_truthy
+      expect(curent_path).to eq(root_path)
+    end
+  end
+end
